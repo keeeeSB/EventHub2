ad62b68eed6a2904fb249f91ce2c2a2c19c82887
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-20 08:54:07 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :name,     presence: true
+  validates :email,    presence: true, uniquness: true
+  validates :password, presence: true
+  validates :bio,      presence: true
 end
