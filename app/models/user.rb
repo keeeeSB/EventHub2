4ef3a066a5814f6dbcf8c1dd5c4cd49080eafe8b
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-20 11:52:53 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   mount_uploader :profile_image, ProfileImageUploader
 
-  validates :name,     presence: true
-  validates :email,    presence: true, uniquness: true
-  validates :password, presence: true
-  validates :bio,      presence: true
+  validates :name,             presence: true
+  validates :email,            presence: true, uniquness: true
+  validates :crypted_password, presence: true
+  validates :bio,              presence: true
 end
