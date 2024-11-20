81cb1b49ad032360789a235c15d03eac60557155
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-20 11:54:01 +0900

    Usersコントローラーにアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,28 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "登録できました。"
+      redirect_to @user
+    else
+      flash.now[:danger] = "登録できませんでした。"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :crypted_password,
+                                   :salt, :prifile_image, :bio)
+    end
 end
