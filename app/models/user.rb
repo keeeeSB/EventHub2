059a02c5e13ec784223edb60cc12d1f67bd74a1e
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-21 09:08:22 +0900

    Userモデルのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 
   validates :name,     presence: true
   validates :email,    presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :bio,      presence: true
 end
