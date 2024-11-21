087aa9f08eda613c58e9d040204c91f9484e884b
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-21 09:07:55 +0900

    Userモデルにnameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_19_234909) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_20_032746) do
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_19_234909) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
