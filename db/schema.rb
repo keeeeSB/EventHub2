e47725e585788db3db86a5cb53fde3c0f263a74c
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-22 12:29:24 +0900

    Eventモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_22_001825) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_22_032817) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "start_time"
+    t.string "location"
+    t.integer "user_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_events_on_category_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_22_001825) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "events", "categories"
+  add_foreign_key "events", "users"
 end
