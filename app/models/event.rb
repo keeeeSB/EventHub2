261f712cba3e7a167c3bfec5f0c85d3e57de4d72
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-25 05:02:55 +0900

    Eventモデルの設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
+  accepts_nested_attributes_for :category
+
   validates :title,       presence: true
   validates :description, presence: true
   validates :start_time,  presence: true
