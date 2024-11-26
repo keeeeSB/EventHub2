27600f14ad6f60f26724d622ee4c5c2a50ed7626
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-27 04:17:38 +0900

    Joinskコントローラーにアクションを追加

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -1,2 +1,21 @@
 class JoinsController < ApplicationController
+  before_action :require_login
+
+  def create
+    @join = current_user.joins.build
+    if @join.save
+      flash[:success] = "参加申し込みをしました。"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "参加申し込みできませんでした。"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @join = Join.find(params[:id])
+    @join.destroy
+    flash[:success] = "参加申し込みを取り消しました。"
+    redirect_to root_path, status: :see_other
+  end
 end
