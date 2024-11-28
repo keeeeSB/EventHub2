122e5bea8c838352e222e4481d1a58b142d4180c
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-11-29 04:34:29 +0900

    お気に入りイベント一覧ページの作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,20 @@
 class FavoritesController < ApplicationController
+  before_action :require_login
+
   def index
+    @favorite_events = current_user.favorites.include(:event).order('events.start_time ASC')
+  end
+
+  def create
+    favorite = current_user.favorites.build(event_id: params[:event_id])
+    flash[:success] = "お気に入り登録しました。"
+    redirect_to root_path
+  end
+
+  def destroy
+    favorite = current_user.favorites.find(params[:id])
+    favorite.destroy!
+    flash[:success] = "お気に入り登録を解除しました。"
+    redirect_to root_path, status: :see_other
   end
 end
